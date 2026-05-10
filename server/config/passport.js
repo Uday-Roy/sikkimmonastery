543c68be0b81dad4c322ee4618340987677a922f
Uday Roy <4321udaybhai0000@gmail.com>
2026-05-10 21:55:19 +0530

    final github oauth fix

--- server/config/passport.js
+++ server/config/passport.js
@@ -99,6 +99,87 @@
 
 // ***   full code for passport.js
 
+// const passport = require("passport");
+
+// const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
+// const GitHubStrategy = require("passport-github2").Strategy;
+
+// const User = require("../models/User");
+
+// // GOOGLE LOGIN
+// passport.use(
+//   new GoogleStrategy(
+//     {
+//       clientID: process.env.GOOGLE_CLIENT_ID,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+
+//       callbackURL: "https://sikkimmonastery.onrender.com/auth/google/callback",
+//     },
+
+//     async (accessToken, refreshToken, profile, done) => {
+//       try {
+//         let user = await User.findOne({
+//           email: profile.emails[0].value,
+//         });
+
+//         if (!user) {
+//           user = await User.create({
+//             name: profile.displayName,
+//             email: profile.emails[0].value,
+//             password: "google-login",
+//           });
+//         }
+
+//         return done(null, user);
+//       } catch (err) {
+//         return done(err, null);
+//       }
+//     },
+//   ),
+// );
+
+// // GITHUB LOGIN
+// passport.use(
+//   new GitHubStrategy(
+//     {
+//       clientID: process.env.GITHUB_CLIENT_ID,
+//       clientSecret: process.env.GITHUB_CLIENT_SECRET,
+
+//       callbackURL: "https://sikkimmonastery.onrender.com/auth/github/callback",
+//     },
+
+//     async (accessToken, refreshToken, profile, done) => {
+//       try {
+//         let user = await User.findOne({
+//           email: profile.emails?.[0]?.value,
+//         });
+
+//         if (!user) {
+//           user = await User.create({
+//             name: profile.displayName || profile.username,
+
+//             email:
+//               profile.emails?.[0]?.value || `${profile.username}@github.com`,
+
+//             password: "github-login",
+//           });
+//         }
+
+//         return done(null, user);
+//       } catch (err) {
+//         return done(err, null);
+//       }
+//     },
+//   ),
+// );
+
+// module.exports = passport;
+
+// ***  full code for passport.js again with github fix issue
+
+// server/config/passport.js
+
 const passport = require("passport");
 
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
@@ -107,11 +188,15 @@ const GitHubStrategy = require("passport-github2").Strategy;
 
 const User = require("../models/User");
 
+// =========================
 // GOOGLE LOGIN
+// =========================
+
 passport.use(
   new GoogleStrategy(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
+
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
 
       callbackURL: "https://sikkimmonastery.onrender.com/auth/google/callback",
@@ -123,44 +208,61 @@ passport.use(
           email: profile.emails[0].value,
         });
 
+        // Create new user if not exists
         if (!user) {
           user = await User.create({
             name: profile.displayName,
+
             email: profile.emails[0].value,
+
             password: "google-login",
           });
         }
 
         return done(null, user);
       } catch (err) {
+        console.log("Google Login Error:", err);
+
         return done(err, null);
       }
     },
   ),
 );
 
+// =========================
 // GITHUB LOGIN
+// =========================
+
 passport.use(
   new GitHubStrategy(
     {
       clientID: process.env.GITHUB_CLIENT_ID,
+
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
 
       callbackURL: "https://sikkimmonastery.onrender.com/auth/github/callback",
+
+      scope: ["user:email", "read:user"],
     },
 
     async (accessToken, refreshToken, profile, done) => {
       try {
+        // GitHub sometimes does not provide email
+        const email =
+          profile.emails && profile.emails.length > 0
+            ? profile.emails[0].value
+            : `${profile.username}@github.com`;
+
         let user = await User.findOne({
-          email: profile.emails?.[0]?.value,
+          email,
         });
 
+        // Create user if not found
         if (!user) {
           user = await User.create({
             name: profile.displayName || profile.username,
 
-            email:
-              profile.emails?.[0]?.value || `${profile.username}@github.com`,
+            email,
 
             password: "github-login",
           });
@@ -168,6 +270,8 @@ passport.use(
 
         return done(null, user);
       } catch (err) {
+        console.log("GitHub Login Error:", err);
+
         return done(err, null);
       }
     },
